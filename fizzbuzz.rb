5118d83d6893d121e35a732db2a75e681bbcb2fc
Marc Poppy <poppy.marc@gmail.com>
2019-04-23 05:08:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(number)
+  if number % 3 == true
+    "Fizz"
+  elsif number % 5 == true
+    "Buzz"
+  elsif number % 3 && 5 == true
+    "FizzBuzz"
+  else
+    "nil"
 end
