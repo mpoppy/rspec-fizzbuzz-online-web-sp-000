7ef8626eb4ad576b2f5ec131dcca456e042ff56c
Marc Poppy <poppy.marc@gmail.com>
2019-04-23 05:28:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if (number % 3) && (number % 5)
+  if number % 3 && number % 5
     "FizzBuzz"
   elsif number % 3
     "Fizz"
