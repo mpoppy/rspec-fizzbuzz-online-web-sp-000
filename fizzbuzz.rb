4bafb3e333f3e941b3922819e31efdba12cb143f
Marc Poppy <poppy.marc@gmail.com>
2019-04-23 05:31:21 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(number)
+  if number % 3 == 00 && number % 5 == 00
+    "FizzBuzz"
+  elsif number % 3 == 00
+    "Fizz"
+  elsif number % 5 == 00
+    "Buzz"
+  end
+end
